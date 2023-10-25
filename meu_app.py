058309982d80f07a8068f22b0eb316f0f508d937
Kevin Morente <morente.kevin@gmail.com>
2023-10-25 00:24:36 -0300

    rev 0024 25102023

--- meu_app.py
+++ meu_app.py
@@ -7,13 +7,14 @@ st.set_page_config(page_title="Meu Site com API")
 
 base_url = 'https://api.github.com'
 
-response = requests.get(url)
-
-st.subheader("Algo com API nesse site test")
-st.title("Dados de alguma aqui aqui")
-
 def selecionarUsuario(username):
     url = f'{base_url}/users/{username}'
     response = requests.get(url)
+    if response.status_code==200:
+        return response.json()
+    else:
+        return None
     
-    return response.json()  
+def ui():
+    st.markdown('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">', unsafe_allow_html=True)
+    st.title('Github Users')
